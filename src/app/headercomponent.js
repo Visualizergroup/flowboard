25c8b8a24119d2bfc3df26fd375f315fcf16a7ce
Sebastian Heinonen <sebastian.heinonen@visualizergroup.com>
2024-01-04 14:35:06 +0200

    infobutton

---
+++ src/app/headercomponent.js
@@ -0,0 +1,42 @@
+"use client"
+import React, { useState } from 'react';
+import { Navbar, Button, Modal } from 'react-bootstrap';
+
+function HeaderComponent() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Navbar bg="light" expand="lg" className="w-100">
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+    <Button variant="primary" onClick={handleShow} className='me-3'>
+      Ohjeita
+    </Button>
+  </Navbar.Collapse>
+</Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Ohjeet flown käyttöä varten</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <p>Aloita kirjoittamalla aloitusteksti(Botin painikkeen tervehdysteksti) Tämän tekstin on tarkoitus toimia herätteenä asiakkaalle.</p>
+        <p>Saat lisättyä elementtejä vetämällä niitä flow alueelle.</p>
+        Saat poistettua elementin valitsemalla elementin ja painamalla delete tai backspace.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default HeaderComponent;
+
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,14 @@ import 'reactflow/dist/style.css';
 import DnDFlow from './flowcomponent';
 import './globals.css'
 import './textupdater.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderComponent from './headercomponent';
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <HeaderComponent></HeaderComponent>
       <DnDFlow />
     </main>
   )
